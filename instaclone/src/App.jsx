e45f0b8ec02f5a08fbbe3649ad65f9efee1d5b3d
oluwadamilola <ooluwadamilola143@gmail.com>
2024-12-19 14:37:31 +0000

    fixing messages

--- instaclone/src/App.jsx
+++ instaclone/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from '../src/pages/Navbar.jsx';
+import Mediamodifier from './pages/Mediamodifier.jsx';
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Mediamodifier />
     </>
   )
 }
